40dee960ee011e558c8f4b94e23bba19bc478b68
SIMJIYEON93 <930515s@gmail.com>
2024-12-23 16:38:10 +0900

    init

---
+++ src/test/java/com/example/sshtunneling/SshTunnelingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sshtunneling;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SshTunnelingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
